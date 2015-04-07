09d69f703841de53572a9e92db2665a2efeee452
Aneesh <aneesh.h.vaidya@gmail.com>
2015-04-07 02:25:28 -0700

    CNodes added.

---
+++ datastructs/CNode.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+public class CNode{
+
+	private int id;
+	private String message;
+	private CNode prev;
+	private boolean head;
+	private Date date;
+
+	public CNode(String message, CNode prev){
+		this.message = message;
+		this.prev = prev;
+		this.head = true;
+		this.id = this.hashCode();
+		this.date = new Date();
+	}
+	
+}
