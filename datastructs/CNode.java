324d9bfce5b80a19b6425ee4991f2c7defea8b50
Aneesh <aneesh.h.vaidya@gmail.com>
2015-04-07 02:36:23 -0700

    CTree file made, ran into some problems of how to implement. (Maybe I'm just tired).

--- datastructs/CNode.java
+++ datastructs/CNode.java
@@ -1,3 +1,5 @@
+package datastructs;
+
 import java.util.Date;
 
 public class CNode{
---
+++ datastructs/CTree.java
@@ -0,0 +1,14 @@
+package datastructs;
+
+import java.util.LinkedList;
+import java.util.HashMap;
+
+public class CTree{
+
+	private HashMap<String, CNode> heads;
+
+	/*
+	We'll talk about how to implement this fully later
+	**/
+
+}
