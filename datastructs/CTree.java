0d7293fbf30fd2d7852e29702ff730ae02c0330f
Aneesh <aneesh.h.vaidya@gmail.com>
2015-04-09 10:17:50 -0700

    CTree barebones working. Initialize should work now when implemented in Gitlet main method.

--- datastructs/CTree.java
+++ datastructs/CTree.java
@@ -1,15 +1,30 @@
 package datastructs;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CTree{
 
 	private HashMap<String, CNode> heads;
-	private CNode current;
+	private ArrayList<String> branchNames;
+	private CNode currentBranch;
 
 	/*
 	We'll talk about how to implement this fully later
 	**/
+
+	public CTree(){
+		this.heads = new HashMap<String, CNode>();
+		this.branchNames = new ArrayList<String>();
+		this.currentBranch = new CNode("initial commit", null);
+	}
+
+	public CNode getCurrentBranch(){
+		return this.currentBranch;
+	}
+
+	public CNode getHeadAtBranch(String branchName){
+		return this.heads.get(branchName);
+	}
 
 }
