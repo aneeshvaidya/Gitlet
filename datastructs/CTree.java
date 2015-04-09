387915bf3906e9c9cef6b6fb2033f26e4efaca30
Aneesh <aneesh.h.vaidya@gmail.com>
2015-04-09 10:27:19 -0700

    CTree updated with methods to support branching

--- datastructs/CTree.java
+++ datastructs/CTree.java
@@ -3,20 +3,23 @@ package datastructs;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+*	This is a CTree class, created to maintain a record of all branch names, and current head pointers
+*	at each branch. The name implies it is a tree, when it is more like a collection of pointers.
+**/
+
 public class CTree{
 
 	private HashMap<String, CNode> heads;
 	private ArrayList<String> branchNames;
-	private CNode currentBranch;
-
-	/*
-	We'll talk about how to implement this fully later
-	**/
+	private String currentBranch;
+	private int branches;
 
 	public CTree(){
 		this.heads = new HashMap<String, CNode>();
 		this.branchNames = new ArrayList<String>();
-		this.currentBranch = new CNode("initial commit", null);
+		this.currentBranch = "master";
+		this.branches = 1;
 	}
 
 	public CNode getCurrentBranch(){
@@ -25,6 +28,12 @@ public class CTree{
 
 	public CNode getHeadAtBranch(String branchName){
 		return this.heads.get(branchName);
+	}
+
+	public void branchTree(String newBranchName){
+		branches++;
+		branchNames.add(newBranchName);
+		heads.put(newBranchName, heads.get(currentBranch));
 	}
 
 }
