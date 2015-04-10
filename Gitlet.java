c0077c55cdc8a488c99d41cfdc8e2f7c8b0cfb8d
Aneesh <aneesh.h.vaidya@gmail.com>
2015-04-10 14:57:36 -0700

    Methods in main to support saving and loading using serializable

--- Gitlet.java
+++ Gitlet.java
@@ -1,4 +1,45 @@
+import datastruct.*;
+
 public class Gitlet {
     public static void main(String[] args) {
+
+
     }
+
+    private static CTree loadCommitTree() {
+		CTree commitTree = null;
+		File commitFile = new File("commitTree.ser");
+		if (commitFile.exists()) {
+            try {
+                FileInputStream fileIn = new FileInputStream(commitFile);
+                ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+                commitTree = (CTree) objectIn.readObject();
+            } catch (IOException e) {
+                String msg = "IOException while loading commit tree.";
+                System.out.println(msg);
+            } catch (ClassNotFoundException e) {
+                String msg = "ClassNotFoundException while loading commit tree.";
+                System.out.println(msg);
+            }	
+    	}
+    	return commitTree;
+	}
+
+	private static void saveCommitTree(CTree commitTree){
+		if(commitTree == null){
+			return;
+		}
+		try {
+            File commitFile = new File("commitTree.ser");
+            FileOutputStream fileOut = new FileOutputStream(commitFile);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+            objectOut.writeObject(commitTree);
+        } catch (IOException e) {
+            String msg = "IOException while saving commit tree.";
+            System.out.println(msg);
+        }
+
+	}
+
+
 }
